7e1b144f8ac652c8c128854d9c247072847038a0
TiagoVeri <tiagoverisjc@gmail.com>
2019-03-29 12:18:22 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -1,12 +1,22 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.nelioalves.cursomc.domain.Cidade;
 import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.domain.Endereco;
+import com.nelioalves.cursomc.domain.enums.TipoCliente;
+import com.nelioalves.cursomc.dto.ClienteDTO;
 import com.nelioalves.cursomc.repositories.ClienteRepository;
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,5 +33,58 @@ public class ClienteService {
 				
 	}
 	
+	//método feito para salvar apenas as informações que queremos
+		//uma forma de manter um determinado dado e alterar outro, sempre com base na regra de negócios
+		public Cliente update(Cliente obj) {
+			Cliente newObj = find(obj.getId());
+			updateData(newObj, obj);
+			return repo.save(newObj);
+		}
+		
+		public void delete(Integer id) {
+			find(id);
+			try {
+				repo.deleteById(id);
+			} 
+			catch(DataIntegrityViolationException e) {
+				throw new DataIntegrityException("Não é possível excluir um Cliente que há entidades relacionadas");
+			}
+			
+		}
+		
+		public List<Cliente> findAll(){
+			return repo.findAll();
+		}
+		
+		public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+			PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+			return repo.findAll(pageRequest);
+		}
+		
+		public Cliente fromDTO(ClienteDTO objDto) {
+			return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		}
+		
+		//método resposável de inserir todo um cadastro de cliente de uma vez
+		public Cliente fromDTO(ClienteNewDTO objDto) {
+			Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
+			Cidade cid = new Cidade(objDto.getCidadeId(),null, null);
+			Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+			cli.getEnderecos().add(end);
+			cli.getTelefones().add(objDto.getTelefone1());
+			if(objDto.getTelefone2()!=null) {
+				cli.getTelefones().add(objDto.getTelefone2());
+			}
+			if(objDto.getTelefone3()!= null) {
+				cli.getTelefones().add(objDto.getTelefone3());
+			}
+			return cli;
+		}	
+		
+		
+		private void updateData(Cliente newObj, Cliente obj){
+			newObj.setNome(obj.getNome());
+			newObj.setEmail(obj.getEmail());
+		}
 	
 }
