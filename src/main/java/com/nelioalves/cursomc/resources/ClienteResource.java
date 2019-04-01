8f33078f6b7f740720d23ff8d922fb0a8d2c35f7
TiagoVeri <tiagoverisjc@gmail.com>
2019-04-01 17:25:59 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
@@ -9,14 +9,13 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc.domain.Cliente;
@@ -56,14 +55,14 @@ public class ClienteResource {
 	}
 	
 	
-	//@PreAuthorize("hasAnyRole('ROLE_ADMIN')") 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')") 
 	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 	
-	//@PreAuthorize("hasAnyRole('ROLE_ADMIN')") 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')") 
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<ClienteDTO>> findAll(){
 		List<Cliente> list = service.findAll();
@@ -71,7 +70,7 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 	
-	//@PreAuthorize("hasAnyRole('ROLE_ADMIN')") 
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')") 
 	@RequestMapping(value="/page", method=RequestMethod.GET)
 	public ResponseEntity<Page<ClienteDTO>> findPage(
 			@RequestParam(value="page", defaultValue="0") Integer page,
