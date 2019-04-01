558655eebfb38dd919b69b3ca8bf45f3d625dd91
TiagoVeri <tiagoverisjc@gmail.com>
2019-04-01 14:58:54 -0300

    Implementando autenticacao e geracao do Token JWT

---
+++ src/main/java/com/nelioalves/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.nelioalves.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.repositories.ClienteRepository;
+import com.nelioalves.cursomc.security.UserSS;
+
+/*
+ * classe de serviço conforme contrato do Spring Security (implements UserDetailsService)
+ * 
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	//Método que recebe user e vai retornar user details.
+	//O email que vai ser o argumento já que user vai ser email
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
