04855a68bb599a92c88105e46717b20b7b76392b
TiagoVeri <tiagoverisjc@gmail.com>
2019-04-01 14:27:34 -0300

    Adicionando configuracoes de Seguranca usando Spring Security

---
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -0,0 +1,92 @@
+package com.nelioalves.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.nelioalves.cursomc.security.JWTAuthenticationFilter;
+import com.nelioalves.cursomc.security.JWTAuthorizationFilter;
+import com.nelioalves.cursomc.security.JWTUtil;
+
+//Classe de configuração para falar o que está liberado ou não por padrão
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	private static final String[] PUBLIC_MATCHERS = {
+			"/h2-console/**"
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**",
+			"/clientes/**"
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/clientes",
+			"/auth/forgot/**"
+	};
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+			}
+		http.cors().and().csrf().disable(); // uma vez que a confi. Cors seja feita, devemos ativar ela
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(HttpMethod.POST,PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	//Sobrecarga para dizer quem é o userDetailDervice e 
+	//quem é o algoritmo de decrypt da senha
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+	
+	//responsável por criptografar as senhas
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
